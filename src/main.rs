81c8de63a9da57c7125768677b4b5eada741318c
Peter Bjorklund <piot@hotmail.com>
2024-08-01 17:21:01 +0200

    chore: add header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/comment-header-rs
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
 use std::fs;
 use std::io::{self};
 use std::path::Path;
